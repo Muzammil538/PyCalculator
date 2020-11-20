7fe5533dc96e46dabf089134aa7282416e28eec3
Muzammil538 <Muzammil538@users.noreply.github.com>
2020-11-20 13:17:50 +0530

    add

---
+++ Calculator-cli.py
@@ -0,0 +1,31 @@
+import math
+
+Uname = input("Enter Your Name:")
+
+Num1 = float(input("Enter a Number:"))
+Num2 = float(input("Enter another Number:"))
+
+print("Choose: +, -, *, /")
+ask = input("What you what to do:")
+
+
+if ask=="+"or "add" or "ADD":
+    print(Num1+Num2)
+
+elif ask=="-" or "minus" or "substract":
+    print(Num1-Num2)
+
+elif ask=="*":
+    print(Num1*Num2)
+
+elif ask=="/":
+    print(Num1/Num2)
+
+else:
+    print(Uname,"Entered",Num1,",",Num2,"Do you want to change")
+
+# Ask_change = print(Uname,"Entered",Num1,",",Num2,"Do you want to change")
+# change = input("If yes(Y)/No(N):")
+
+# if change=="Y"or "y" or "yes" or "Yes":
+#     print("Hello")
